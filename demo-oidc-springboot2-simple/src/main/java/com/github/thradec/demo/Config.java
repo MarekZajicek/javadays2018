0cc970a2fb7c39fd3c44b3b670f8c054a98c732f
Tomas Hradec <thradec@gmail.com>
2018-10-31 16:20:13 +0100

    1. oidc login

--- demo-oidc-springboot2-simple/src/main/java/com/github/thradec/demo/Config.java
+++ demo-oidc-springboot2-simple/src/main/java/com/github/thradec/demo/Config.java
@@ -8,7 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class Config extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.oauth2Login()
+                .loginPage("/oauth2/authorization/demo")
+                .and()
+            .authorizeRequests()
+                .anyRequest()
+                    .authenticated();
     }
 
 }
